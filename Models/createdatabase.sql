af57fcfb6b3f684aa4495de61c79f3ec02cbf59c
Colan Connon <cconnon11@gmail.com>
2016-08-09 16:18:29 -0400

    added database create statement

---
+++ Models/createdatabase.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE `PhpFinal` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE TABLE `Users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
