efdf0d83981ca97c787e015e0da843038cf9a804
Colan Connon <cconnon11@gmail.com>
2016-08-10 15:57:08 -0400

    Finished mocking database repository object

--- tests/UserDatabaseTests.php
+++ tests/UserDatabaseTests.php
@@ -17,7 +17,9 @@ class UserDatabaseTest extends TestCase
         
         $statement = $connection->prepare('Delete From Users');
         $statement->execute();
-        
+        $this->setUpMocking();
+    }
+    public function setUpMocking(){
         $faker = Faker\Factory::create();
         $this->userDatabaseRepositoryMock = m::mock('UserDatabaseRepository');
         $user = new User();
@@ -26,47 +28,60 @@ class UserDatabaseTest extends TestCase
         $user->lastName = $faker->lastName;
         $user->age= $faker->randomDigitNotNull;
         $user->id = 1;
-        
-        $user = new User();
-        $user->username = $faker->userName;
-        $user->firstName = $faker->firstName;
-        $user->lastName = $faker->lastName;
-        $user->age= $faker->randomDigitNotNull;
-        $user->id = 1;
-        $usersArray = [$user];
-        $this->userDatabaseRepositoryMock->shouldReceive('FindAll')->andReturn($usersArray);
-        $this->userDatabaseRepositoryMock->shouldReceive('Find')->andReturn($user);
+        $this->usersArray = [$user];
+        $this->userDatabaseRepositoryMock->shouldReceive('FindAll')->andReturnUsing(function(){
+            return $this->usersArray;
+        });
+        $this->userDatabaseRepositoryMock->shouldReceive('Find')->andReturnUsing(function($id) {
+            foreach($this->usersArray as $row) {
+                if($row->id == $id){
+                    return $row;
+                }
+            }
+            return null;
+        });
         $this->userDatabaseRepositoryMock->shouldReceive('Save')->andReturnUsing(function($user){
             if($user->validate()) {
                 if(empty($user->id)){
-                    $user->id = 1;
+                    $user->id = 2;
+                    array_push($this->usersArray, $user);
                 }
                 return true;
             } else {
                 return false;
             }
         });
-        
-        
+        $this->userDatabaseRepositoryMock->shouldReceive('Destroy')->andReturnUsing(function($id){
+            foreach($this->usersArray as $row) {
+                if($row->id == $id){
+                    $userKey = array_search($row, $this->usersArray);
+                    if($userKey != null) {
+                        unset($this->usersArray[$userKey]);
+                    }
+                }
+            }
+            
+            
+        });
     }
     public function testDatabaseOperations(){
         $faker = Faker\Factory::create();
-        $this->assertTrue(empty($this->userDatabaseRepo->findAll()));
+        $this->assertTrue(empty($this->userDatabaseRepo->FindAll()));
         $user = new User();
         $user->username = $faker->userName;
         $user->firstName = $faker->firstName;
         $user->lastName = $faker->lastName;
         $user->age= $faker->randomDigitNotNull;
-        $this->userDatabaseRepo->save($user);
-        $this->assertEquals(count($this->userDatabaseRepo->findAll()), 1);
+        $this->userDatabaseRepo->Save($user);
+        $this->assertEquals(count($this->userDatabaseRepo->FindAll()), 1);
         
         $user->firstName = $faker->userName;
-        $this->userDatabaseRepo->save($user);
-        $this->assertEquals(count($this->userDatabaseRepo->findAll()), 1);
+        $this->userDatabaseRepo->Save($user);
+        $this->assertEquals(count($this->userDatabaseRepo->FindAll()), 1);
         
-       
-        $this->userDatabaseRepo->destroy($user->id);
-        $this->assertEquals(count($this->userDatabaseRepo->findAll()), 0);
+        
+        $this->userDatabaseRepo->Destroy($user->id);
+        $this->assertEquals(count($this->userDatabaseRepo->FindAll()), 0);
     }
     
     public function testMockDatabaseOperations() {
@@ -80,7 +95,15 @@ class UserDatabaseTest extends TestCase
         $user->lastName = $faker->lastName;
         $user->age= $faker->randomDigitNotNull;
         
-        print_r($this->userDatabaseRepositoryMock->Save($user));
+        $this->assertEquals($this->userDatabaseRepositoryMock->Save($user), true);
+        $this->assertEquals($this->userDatabaseRepositoryMock->Find($user->id)->firstName, $user->firstName);
+        //update username
+        $oldUsername = $user->username;
+        $user->username = 'test';
+        $this->assertEquals($this->userDatabaseRepositoryMock->Save($user), true);
+        $this->assertNotEquals($this->userDatabaseRepositoryMock->Find($user->id), $oldUsername);
+        $this->userDatabaseRepositoryMock->Destroy(2);
+        $this->assertEquals(count($this->userDatabaseRepositoryMock->FindAll()), 1);
     }
     
 }
